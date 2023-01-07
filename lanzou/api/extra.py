bae3c8bd7bedf9075e0db68edad2ca88fdebc410
Leon406 <deadogone@gmail.com>
2023-01-07 15:50:15 +0800

    feat(api): 短网址改用4个生成长期的

--- lanzou/api/extra.py
+++ lanzou/api/extra.py
@@ -7,42 +7,55 @@ from lanzou.debug import logger
 
 timeout = 2
 
-# 不登录一天
-short_url_server = "https://tturl.cn/api/buildDwz"
-
-short_url_server2 = "http://create.link66.cn/createBySingle"
-
 
 def get_short_url(url: str):
     """短链接生成器"""
     headers = {'User-Agent': USER_AGENT}
     short_url = ""
     try:
-        post_data = {
-            "url": url,
-            "type": 1
-        }
-
-        resp = requests.post(short_url_server, data=post_data, verify=False, headers=headers, timeout=timeout)
+        post_data = {"url": url}
+        # 10W次/天, https证书过期
+        headers["Authorization"] = "Token xxHQfao69Ra9G7EI87mC"
+        resp = requests.post("https://www.dwz.lc/api/url/add", json=post_data, headers=headers, timeout=timeout)
         logger.error(resp.text)
         rsp = json.loads(resp.text)
         if rsp:
-            short_url = rsp["data"][0]
+            short_url = rsp["short"]
     except Exception as e:
         logger.error(f"get_short_url error: e={e}")
 
     if not short_url:
-        headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
-        post_data = {
-            "longUrl": url,
-            "domain": "1v9.xyz",
-            "expireType": 1
-        }
         try:
-            html = requests.post(short_url_server2, data=post_data, verify=False, headers=headers, timeout=timeout).text
-            rsp = json.loads(html)
+            post_data = {"url": url}
+            # 10W次/天, https证书过期
+            headers["Authorization"] = "Token cH4lpSuC6LgqoDidiqB5"
+            resp = requests.post("https://www.ecx.cx/api/url/add", json=post_data, headers=headers, timeout=timeout)
+            logger.error(resp.text)
+            rsp = json.loads(resp.text)
             if rsp:
-                short_url = rsp["data"]
+                short_url = rsp["short"]
+        except Exception as e:
+            logger.error(f"get_short_url error: e={e}")
+
+    if not short_url:
+        try:
+            # https, 国外,速度较慢
+            resp = requests.get(f"https://tinyurl.com/api-create.php?url={url}", headers=headers, timeout=timeout)
+            logger.error(resp.text)
+            if resp.text and len(resp.text) < 32:
+                short_url = resp.text
+        except Exception as e:
+            logger.error(f"get_short_url error: e={e}")
+
+    if not short_url:
+        headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
+        post_data = {"long_url": url}
+        try:
+            # 支持https, 国外,速度较慢
+            resp = requests.post("http://gg.gg/create", data=post_data, headers=headers, timeout=timeout).text
+            logger.error(resp)
+            if resp:
+                short_url = resp
         except Exception as e:
             logger.error(f"get_short_url2 error: e={e}")
 
